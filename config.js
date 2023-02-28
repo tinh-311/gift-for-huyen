fa9d40dc01990763945e7c134c4a1681a123cda8
timmo <timmo.n@itcgroup.io>
2023-02-28 08:19:18 +0700

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/ntt.tinh311';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 08/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'img.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
